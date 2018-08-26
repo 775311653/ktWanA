f1b4c2978d28ba3cfde862d620412255970f8ce4
xieping <775311653>
2018-08-26 09:08:05 +0800

    载入页

---
+++ app/src/main/java/com/mohe/ktwana/ui/SplashActivity.kt
@@ -0,0 +1,53 @@
+package com.mohe.ktwana.ui
+
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.blankj.utilcode.util.ActivityUtils
+import com.mohe.ktwana.MainActivity
+import com.mohe.ktwana.R
+import com.mohe.ktwana.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : BaseActivity() {
+    override fun attachLayoutRes(): Int {
+        return R.layout.activity_splash
+    }
+
+    override fun initData() {
+    }
+
+    override fun initView() {
+        showSplashView()
+    }
+
+    override fun start() {
+    }
+
+    private fun showSplashView() {
+        var anim = ObjectAnimator.ofFloat(splash_rl_parent, "alpha", 0.3f, 1.0f)
+                .setDuration(2000)
+        anim.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                ActivityUtils.startActivity(MainActivity::class.java)
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                finish()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+            }
+        })
+        anim.start()
+    }
+
+
+}
